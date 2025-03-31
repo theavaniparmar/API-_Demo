d5c0bf592c1030c7cd99ff747c54cc4f1844672a
Roma Gorfad <roma@evisionits.com>
2025-03-31 11:24:58 +0530

    Add project files.

---
+++ API _Demo/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace API__Demo
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Double Salary { get; set; }
+    }
+}
